cac5f9327ac3c9c0e2d9923f809f7d28720ff799
liquid8796 <hanam.tranle.5@gmail.com>
2020-02-20 23:19:17 +0700

    20/02

--- Data Preprocessing/Data_Preprocessing.py
+++ Data Preprocessing/Data_Preprocessing.py
@@ -5,11 +5,21 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('C:\\Users\\Liquid\\Source\\repos\\machine_learning\\Data Preprocessing\\Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+Y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = np.nan, strategy = "mean",)
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
