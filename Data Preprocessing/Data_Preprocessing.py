f34af2b010acc45ccddfdc022a7abd1621bbcd38
liquid8796 <hanam.tranle.5@gmail.com>
2020-02-22 00:23:22 +0700

    22/02

--- Data Preprocessing/Data_Preprocessing.py
+++ Data Preprocessing/Data_Preprocessing.py
@@ -5,14 +5,13 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('C:\\Users\\Liquid\\Source\\repos\\machine_learning\\Data Preprocessing\\Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
-print(X)
 
 # Encoding categorical data
 from sklearn.preprocessing import OneHotEncoder
@@ -21,4 +20,18 @@ ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrou
 X = np.array(ct.fit_transform(X), dtype=np.float)
 # Encoding the Dependent Variable
 from sklearn.preprocessing import LabelEncoder
-Y = LabelEncoder().fit_transform(Y)
+y = LabelEncoder().fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train.reshape(-1, 1))
+#y_test = sc_y.transform(y_test.reshape(-1, 1))
+
